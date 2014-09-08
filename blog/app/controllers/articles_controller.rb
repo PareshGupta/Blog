4e1efde66dbe7e501869bad55fda152ff9e549bc
Paresh <paresh@sahil-Vostro-3560.(none)>
2014-09-08 17:14:44 +0530

    add edit and update functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
-
+ 
   def create
     @article = Article.new(article_params)
  
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
@@ -17,9 +25,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  private
-  def article_params
-    params.require(:article).permit(:title, :text)
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
